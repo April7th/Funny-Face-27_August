1a6fc8a09d3c4d668e42ff8b9eddfe157a6aff4f
LeDuyTan <ldtapril7th@gmail.com>
2024-09-19 17:51:47 +0700

    first commit

---
+++ funnyface2222/screen/mainscreen/Video/cell/newvideo/newVideo2Swap.swift
@@ -0,0 +1,29 @@
+//
+//  newVideo2Swap.swift
+//  funnyface2222
+//
+//  Created by Lê Duy Tân on 19/9/24.
+//
+
+import UIKit
+
+class newVideo2Swap: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
